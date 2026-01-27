7b7956f9137969ef26c37a4355a9cd1547998e2a
Nailyn <ntabeldinov2002@gmail.com>
2026-01-27 06:58:06 +0500

    feat(api): add types and schemas for client in children-clients

--- shared/src/children-client.ts
+++ shared/src/children-client.ts
@@ -18,3 +18,17 @@ export const CreateChildRequestSchema = schemas.Children_CreateChildRequest;
 export const CreateChildResponseSchema = schemas.Children_ChildResponse;
 export const UpdateChildRequestSchema = schemas.Children_UpdateChildRequest;
 export const UpdateChildResponseSchema = schemas.Children_ChildUpdateResponse;
+
+export type CreateClientRequestDto = z.infer<
+  typeof schemas.Clients_CreateClientRequest
+>;
+export type CreateChildResponseDto = z.infer<typeof schemas.Clients_Client>;
+export type UpdateClientRequestDto = z.infer<
+  typeof schemas.Clients_UpdateClientRequest
+>;
+export type UpdateClientResponseDto = z.infer<typeof schemas.Clients_Client>;
+
+export const CreateClientRequestSchema = schemas.Clients_CreateClientRequest;
+export const CreateClientResponseSchema = schemas.Clients_Client;
+export const UpdateClientRequestSchema = schemas.Clients_UpdateClientRequest;
+export const UpdateClientResponseSchema = schemas.Clients_Client;
